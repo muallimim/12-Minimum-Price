0daad959710b867a6a8d66cac606f5f0374896f1
muallimim <muallimim@yahoo.com>
2023-01-17 22:01:17 +0300

    proje tamamlandı

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
-console.log('Hello');
+document.getElementById('find_minimum').addEventListener('click', find_minimum)
+
+
+function find_minimum(){
+    event.preventDefault();
+
+    var price1;
+    var price2;
+    var price3;
+    var min;
+    var max;
+
+    price1 = document.getElementById('price1').value;
+    price2 = document.getElementById('price2').value;
+    price3 = document.getElementById('price3').value;
+
+    max = price1;
+
+    if(parseInt(price2) > parseInt(max))
+    max = price2;
+
+    if(parseInt(price3) > parseInt(max))
+    max = price3;
+
+
+    document.getElementById('result').innerHTML = max;
+
+    
+
+ 
+
+
+
+
+}
